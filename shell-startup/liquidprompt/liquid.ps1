535fac369d5c45147a5117f5748ec396da363450
Haziq Noor <haziq@protonmail.com>
2017-10-14 13:29:20 +0800

    Simplified shell prompt

--- shell-startup/liquidprompt/liquid.ps1
+++ shell-startup/liquidprompt/liquid.ps1
@@ -29,11 +29,11 @@
 # see the README.
 
 # add time, jobs, load and battery
-LP_PS1="${LP_PS1_PREFIX}${LP_TIME}${LP_BATT}${LP_LOAD}${LP_JOBS}"
+LP_PS1="${LP_PS1_PREFIX}${LP_JOBS}"
 # add user, host and permissions colon
 LP_PS1="${LP_PS1}${LP_BRACKET_OPEN}${LP_USER}${LP_HOST}${LP_PERM}"
 
-LP_PS1="${LP_PS1}${LP_PWD}${LP_BRACKET_CLOSE}${LP_VENV}${LP_PROXY}"
+LP_PS1="${LP_PS1}${LP_PWD}${LP_BRACKET_CLOSE}${LP_VENV}"
 
 # Add VCS infos
 # If root, the info has not been collected unless LP_ENABLE_VCS_ROOT
@@ -41,7 +41,6 @@ LP_PS1="${LP_PS1}${LP_PWD}${LP_BRACKET_CLOSE}${LP_VENV}${LP_PROXY}"
 LP_PS1="${LP_PS1}${LP_VCS}"
 
 # add return code and prompt mark
-# LP_PS1="${LP_PS1}${LP_RUNTIME}${LP_ERR}${LP_MARK_PREFIX}${LP_MARK}${LP_PS1_POSTFIX}"
 LP_PS1="${LP_PS1}${LP_RUNTIMR}${LP_MARK_PREFIX}${LP_MARK}${LP_PS1_POSTFIX}"
 
 # "invisible" parts
